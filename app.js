0d9c77aeffc6cad2dceda6d46d477453dd7d7d68
facundocenteno <75740251+facundocenteno@users.noreply.github.com>
2021-06-10 20:10:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const app = express();
 const bodyParser = require("body-parser");
 
 
-app.use(bodyParser.urlencoded({extended:true})); // esa es una linea de codigo que tenemos que poner siempre
-                                                  //cuando queremos parse into the body of the post request
+app.use(bodyParser.urlencoded({extended:true})); 
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -24,7 +23,7 @@ app.post("/", function(req,res){
     response.on("data", function(data) {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description; //pongo [0] pq esta dentro del primer array del objeto.
+      const weatherDescription = weatherData.weather[0].description; 
       const icon = weatherData.weather[0].icon;
       const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
